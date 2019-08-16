37d336a0e0f50dd30cd9428eba12650ac957a3fe
Brittany Javalera <brittjavalera@gmail.com>
2019-08-16 17:29:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,8 +13,8 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM series 
-  JOIN authors ON series.author_id = author.id 
-  JOIN subgrenres ON series.subgenre_id = subgenre.id;"
+  JOIN authors ON series.author_id = authors.id 
+  JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
