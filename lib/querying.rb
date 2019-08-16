a107e2cede64425fdf350407e7482fea2ada327b
Brittany Javalera <brittjavalera@gmail.com>
2019-08-16 18:05:51 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,5 +26,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT name, SUM("
+  "SELECT name, COUNT(book_id) FROM characters
+  JOIN charac"
 end
