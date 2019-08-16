7f12f28a3afdb775220ecdea8940bae00a15bfd2
Brittany Javalera <brittjavalera@gmail.com>
2019-08-16 17:51:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT title FROM series
   JOIN authors ON series.author_id = authors.id
-  JOIN characters ON authors.character_id"
+  JOIN characters ON authors.id = characters.author_id
+  HAVING "
 end
 
 def select_character_names_and_number_of_books_they_are_in
